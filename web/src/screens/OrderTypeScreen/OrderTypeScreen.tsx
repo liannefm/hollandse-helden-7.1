c4c0ca06242b829c8c31a0ea5b7b8d78f16f2746
Tom Tiedemann <tomtiedemann30@gmail.com>
2026-02-06 21:15:06 +0100

    bug gefixt

--- web/src/screens/OrderTypeScreen/OrderTypeScreen.tsx
+++ web/src/screens/OrderTypeScreen/OrderTypeScreen.tsx
@@ -1,10 +1,10 @@
 import './OrderTypeScreen.scss';
 import background from '../../assets/images/background.png';
-import tasje from '../../assets/images/tasje.png';
-import tafeltje from '../../assets/images/tafeltje.png';
-import vlagnl from '../../assets/images/vlagnl.png';
-import vlagen from '../../assets/images/vlagen.png';
-import vlagde from '../../assets/images/vlagde.png';
+import tasje from '../../assets/images/icons/tasje.png';
+import tafeltje from '../../assets/images/icons/tafeltje.png';
+import vlagnl from '../../assets/images/icons/vlagnl.png';
+import vlagen from '../../assets/images/icons/vlagen.png';
+import vlagde from '../../assets/images/icons/vlagde.png';
 
 export default function OrderTypeScreen() {
     return (
@@ -18,12 +18,12 @@ export default function OrderTypeScreen() {
 
                 <div className='buttonbox'>
                     <button id='eatin'>
-                        <img src={tafeltje} id="tafeltje"/>
+                        <img src={tafeltje} id="tafeltje" />
                         Eat in
                     </button>
 
                     <button id='takeaway'>
-                        <img src={tasje} id="tasje"/>
+                        <img src={tasje} id="tasje" />
                         Take away
                     </button>
                 </div>
@@ -31,17 +31,17 @@ export default function OrderTypeScreen() {
             <footer>
                 <div className='buttonboxtaal'>
                     <button id="nl">
-                        <img src={vlagnl} id="vlagnl"/>
+                        <img src={vlagnl} id="vlagnl" />
                         Nederlands
                     </button>
 
                     <button id="en">
-                        <img src={vlagen} id="vlagen"/>
+                        <img src={vlagen} id="vlagen" />
                         English
                     </button>
 
                     <button id="de">
-                        <img src={vlagde} id="vlagde"/>
+                        <img src={vlagde} id="vlagde" />
                         Deutsch
                     </button>
                 </div>
