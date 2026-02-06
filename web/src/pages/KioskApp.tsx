a89d866b33394b3e1772c6f45a78c6c5e1f9179b
Tom Tiedemann <tomtiedemann30@gmail.com>
2026-02-06 21:00:04 +0100

    PaymentInProgressScreen gemaakt

--- web/src/pages/KioskApp.tsx
+++ web/src/pages/KioskApp.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import IdleScreen from "../screens/IdleScreen/IdleScreen.tsx";
 import MenuScreen from "../screens/MenuScreen/MenuScreen.tsx";
 import ProductDetailScreen from "../screens/ProductDetailScreen/ProductDetailScreen.tsx";
+import PaymentInProgressScreen from "../screens/PaymentInProgressScreen/PaymentInProgressScreen.tsx";
 
 function KioskApp() {
-    const [screen, setScreen] = useState("product-detail");
+    const [screen, setScreen] = useState("payment-in-progress");
     // const [order, setOrder] = useState<any[]>([]);
 
     return (
@@ -13,6 +14,7 @@ function KioskApp() {
             {screen === "idle" && <IdleScreen onStart={() => setScreen("menu")} />}
             {screen === "menu" && <MenuScreen />}
             {screen === "product-detail" && <ProductDetailScreen />}
+            {screen === "payment-in-progress" && <PaymentInProgressScreen />}
         </>
     );
 }
---
+++ web/src/screens/PaymentInProgressScreen/PaymentInProgressScreen.tsx
@@ -0,0 +1,19 @@
+import './PaymentInProgressScreen.scss';
+
+import logo from "../../assets/images/logos/logo.webp";
+import background from "../../assets/images/background.png";
+
+export default function PaymentInProgressScreen() {
+    return (
+        <div className="payment-in-progress-screen">
+            <header>
+                <img src={logo} alt="Logo" />
+            </header>
+            <main>
+                <img className="background" src={background} alt="Background" />
+
+                <h2>follow the instructions on the payment terminal</h2>
+            </main>
+        </div>
+    );
+}
