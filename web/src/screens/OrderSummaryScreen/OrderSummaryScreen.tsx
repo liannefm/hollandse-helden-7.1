5eaec9d0332e86aec97147667a2a84bc396c10a5
Tom Tiedemann <tomtiedemann30@gmail.com>
2026-02-08 16:14:27 +0100

    laaste paar erros gefixt en scrollbar toegevoegd

--- web/src/screens/OrderSummaryScreen/OrderSummaryScreen.tsx
+++ web/src/screens/OrderSummaryScreen/OrderSummaryScreen.tsx
@@ -16,29 +16,27 @@ export default function OrderSummaryScreen() {
             <main>
                 <img className='background' src={background} />
 
+                <div className="products">
+                    <div className="product">
+                        <img src={template1} />
+                        <div className="quantity-and-remove">
+                            <div className="quantity">
+                                <button className="subtract-button" />
+                                <p>1</p>
+                                <button className="add-button" />
+                            </div>
 
-                <div className='grotebox'>
-                    <img src={template1} id="template1"></img>
-
-                    <div className='box1'>
-                        <div className="quantity">
-                            <button className="subtract-button" />
-                            <p>1</p>
-                            <button className="add-button" />
+                            <button id="removeitem">Remove item</button>
                         </div>
-
-                        <button id="removeitem">Remove item</button>
-
-                    </div>
-
-                    <div className='box2'>
-                        <h2>€7.50</h2>
-                        <h2>320 kcal</h2>
+                        <div className="price-and-calories">
+                            <p>€7.50</p>
+                            <p>320 kcal</p>
+                        </div>
                     </div>
                 </div>
 
+
             </main>
-                <div id="lijn1"></div>
 
             <footer>
                 <div className='box1footer'>
